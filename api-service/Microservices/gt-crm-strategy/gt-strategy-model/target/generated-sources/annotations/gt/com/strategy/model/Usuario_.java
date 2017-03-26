dbc66fcfb07f7f62f59c548769ed6e669d83e371
weslyFernandez <wesly_fernandez1@outlook.com>
2017-03-26 17:50:19 -0600

    ADD: Prototipo

---
+++ api-service/Microservices/gt-crm-strategy/gt-strategy-model/target/generated-sources/annotations/gt/com/strategy/model/Usuario_.java
@@ -0,0 +1,20 @@
+package gt.com.strategy.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-26T16:47:05")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, BigDecimal> id;
+    public static volatile SingularAttribute<Usuario, String> estado;
+    public static volatile SingularAttribute<Usuario, String> usuario;
+    public static volatile SingularAttribute<Usuario, String> email;
+    public static volatile SingularAttribute<Usuario, String> contrasenia;
+    public static volatile SingularAttribute<Usuario, Date> fechaCreacion;
+
+}
